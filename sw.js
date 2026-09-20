23b792c9986dde671a615e6f6985d988ef715584
Carouan <baudoux.sebastien@gmail.com>
2026-09-20 12:51:38 +0200

    feat(v0.1): add sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="glom-v0.1.0";
+const CORE=["./","./index.html","./styles.css","./manifest.webmanifest","./icons/icon.svg","./src/app.js"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{if(r&&r.ok&&r.type!=="opaque"){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return r;})));});
