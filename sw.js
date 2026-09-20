55d46a096ecd12c81e5df3cb58dcd23d0f262579
Carouan <baudoux.sebastien@gmail.com>
2026-09-20 13:07:11 +0200

    chore: refresh PWA cache for v0.1.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="glom-v0.1.0";
+const CACHE="glom-v0.1.1";
 const CORE=["./","./index.html","./styles.css","./manifest.webmanifest","./icons/icon.svg","./src/app.js"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
