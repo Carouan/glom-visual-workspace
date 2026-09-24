b048b1f948a4be474977a66418d71f3b726668dc
Carouan <baudoux.sebastien@gmail.com>
2026-09-24 19:47:16 +0200

    chore: cache v0.3.19 occurrence helpers

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="glom-v0.3.18";
-const CORE=["./","./index.html","./styles.css","./manifest.webmanifest","./icons/icon.svg","./src/app.js","./src/viewers/index.js","./src/exporters/mindmap.js","./src/exporters/workspace.js","./src/workspaces/recent.js","./src/workspaces/settings.js","./src/mindmap/layouts.js"];
+const CACHE="glom-v0.3.19";
+const CORE=["./","./index.html","./styles.css","./manifest.webmanifest","./icons/icon.svg","./src/app.js","./src/viewers/index.js","./src/exporters/mindmap.js","./src/exporters/workspace.js","./src/workspaces/recent.js","./src/workspaces/settings.js","./src/mindmap/layouts.js","./src/mindmap/occurrences.js"];
 
 async function precache(){
   const cache=await caches.open(CACHE);
